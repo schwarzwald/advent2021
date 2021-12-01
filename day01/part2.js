79ed6be4a44cadc8dc264f9467d8c56fbbc38eb5
bibo <bibaso@gmail.com>
2021-12-01 14:43:19 +0100

    Simplify day 1 - part 2 solution

--- day01/part2.js
+++ day01/part2.js
@@ -2,4 +2,4 @@ module.exports = input =>
   input.split('\n')
     .map(Number)
     .reduce((sum, _, i, arr) => i - 2
-      && arr[i - 1] + arr[i - 2] + arr[i - 3] < arr[i] + arr[i - 1] + arr[i - 2] ? sum + 1 : sum, 0);
+      && arr[i - 3] < arr[i] ? sum + 1 : sum, 0);
