53debb86d728bdf87d7b506401cb7b7334b74ad6
bibo <bibaso@gmail.com>
2021-12-03 20:00:43 +0100

    Simplify Day 3 part 2

--- day03/part2.js
+++ day03/part2.js
@@ -1,15 +1,13 @@
 module.exports = input => {
   const reduce = (codes, high, low) => {
-    let reduced = codes.slice();
-
     for (let i = 0; i < codes[0].length; i++) {
-      let count = reduced.reduce((s, c) => s + +c[i], 0);
-      let b = count * 2 >= reduced.length ? high : low;
+      let count = codes.reduce((s, c) => s + +c[i], 0);
+      let b = count * 2 >= codes.length ? high : low;
 
-      reduced = reduced.filter(c => c[i] == b);
+      codes = codes.filter(c => c[i] == b);
 
-      if (reduced.length == 1) {
-        return parseInt(reduced[0], 2);
+      if (codes.length == 1) {
+        return parseInt(codes[0], 2);
       }
     }
   }
