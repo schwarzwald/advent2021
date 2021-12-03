3d9a5154f6d6547cb92b1f79d8a33d4b82a08f60
bibo <bibaso@gmail.com>
2021-12-03 19:17:06 +0100

    Simplify Day 3 part 2

--- day03/part2.js
+++ day03/part2.js
@@ -1,36 +1,20 @@
 module.exports = input => {
-  const countBits = (arr, i) => {
-    let result = 0;
+  const reduce = (codes, high, low) => {
+    let reduced = codes.slice();
 
-    for (let code of arr) {
-      result += +code[i];
+    for (let i = 0; i < codes[0].length; i++) {
+      let count = reduced.reduce((s, c) => s + +c[i], 0);
+      let b = count * 2 >= reduced.length ? high : low;
+
+      reduced = reduced.filter(c => c[i] == b);
+
+      if (reduced.length == 1) {
+        return parseInt(reduced[0], 2);
+      }
     }
-
-    return result;
   }
+
   let codes = input.split('\n').map(t => t.trim());
 
-  let oxygenCodes = codes.slice();
-  for (let i = 0; i < codes[0].length; i++) {
-    let count = countBits(oxygenCodes, i);
-    let x = count * 2 >= oxygenCodes.length ? '1' : '0';
-
-    oxygenCodes = oxygenCodes.filter(c => c[i] == x);
-    if (oxygenCodes.length == 1) {
-      break;
-    }
-  }
-
-  let co2Codes = codes.slice();
-  for (let i = 0; i < codes[0].length; i++) {
-    let count = countBits(co2Codes, i);
-    let x = count * 2 >= co2Codes.length ? '0' : '1';
-
-    co2Codes = co2Codes.filter(c => c[i] == x);
-    if (co2Codes.length == 1) {
-      break;
-    }
-  }
-
-  return parseInt(oxygenCodes[0], 2) * parseInt(co2Codes[0], 2);
+  return reduce(codes, '1', '0') * reduce(codes, '0', '1');
 }
