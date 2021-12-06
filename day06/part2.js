40f638c96dc167cb87bb2a86effa07bbc4ccaa70
bibo <bibaso@gmail.com>
2021-12-06 21:05:44 +0100

    Day 6 part 2 improvment

--- day06/part2.js
+++ day06/part2.js
@@ -1,29 +1,16 @@
-const cache = new Map();
-const id = (s, d) => `${s}#${d}`;
-
-const population = (start, days) => {
-  if (days <= 0) {
-    return 0;
-  }
-
-  let popId = id(start, days);
-  if (cache.get(popId)) {
-    return cache.get(popId);
-  }
-
-  let periods = Math.ceil((days - start) / 7);
-  let sum = periods;
-
-  for (let i = 0; i < periods; i++) {
-    sum += population(6, days - start - 3 - 7 * i);
-  }
-
-  cache.set(popId, sum);
-  return sum;
-}
-
-module.exports = input =>
-  input.split(',')
+module.exports = input => {
+  let populations = input.split(',')
     .map(Number)
-    .map(f => 1 + population(f, 256))
-    .reduce((sum, a) => sum + a);
+    .reduce((arr, c) => {
+      arr[c]++;
+      return arr;
+    }, [...new Array(9)].map(_ => 0));
+
+  for (let i = 0; i < 256; i++) {
+    let newborn = populations.shift();
+    populations.push(newborn);
+    populations[6] += newborn;
+  }
+
+  return populations.reduce((sum, a) => sum + a);
+}
